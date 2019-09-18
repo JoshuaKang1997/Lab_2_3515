be078678577d8ff73e6d2ed67775fad8cf68f669
JoshuaKang1997 <joshkang1997@gmail.com>
2019-09-18 10:47:03 -0400

    added if statements to check conditions

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,13 +16,34 @@ public class FormActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //button object declaration
         Button button = (Button) findViewById(R.id.button);
+        //EditText Object declaration
+        final EditText nameText = (EditText) findViewById(R.id.nameText);
+        final EditText emailText = (EditText) findViewById(R.id.emailText);
+        final EditText pText = (EditText) findViewById(R.id.pText);
+        final EditText pcText = (EditText) findViewById(R.id.pcText);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (nameText.getText().toString().matches("") ||
+                        emailText.getText().toString().matches("") ||
+                        pText.getText().toString().matches("") ||
+                        pcText.getText().toString().matches("")) {
 
-                Toast.makeText(getApplicationContext(),"you have clicked the button"
-                        , Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "You have left a field empty!"
+                            , Toast.LENGTH_LONG).show();
+                } else {
+                    if (!pText.getText().toString().matches(pcText.getText().toString())) {
+                        Toast.makeText(getApplicationContext(), "Your passwords don't match!"
+                                , Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Welcome, " + nameText.getText().toString()
+                                        + ", to the SignUpForm App!"
+                                , Toast.LENGTH_LONG).show();
+                    }
+                }
             }
         });
     }
